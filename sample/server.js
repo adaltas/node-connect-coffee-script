ca5304939509ed3df8863b581d2565f156ef1e74
David Worms <david@adaltas.com>
2012-07-23 21:28:16 +0200

    Initial release

---
+++ sample/server.js
@@ -0,0 +1,25 @@
+
+// npm install connect
+// node server.js
+// curl http://localhost:3000/test.js
+
+var coffeeScript = require('coffee-script');
+var connectCoffeeScript = require('..');
+var connect = require('connect');
+
+var app = connect();
+
+function compile(str, options) {
+    options.bare = true;
+    return coffeeScript.compile(str, options);
+}
+
+app.use(connectCoffeeScript({
+    src: __dirname,
+    dest: __dirname + '/public',
+    compile: compile
+}));
+
+app.use(connect.static(__dirname + '/public'));
+
+app.listen(3000)
